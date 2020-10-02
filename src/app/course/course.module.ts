3e5919630805327ac5d0f227948e1a498a62dd23
Clifton <clifton.baggerman@me.com>
2020-10-02 12:58:35 +1000

    added CourseService to course.module

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CourseService } from './services/course.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [CourseService],
 })
-export class CourseModule { }
+export class CourseModule {}
