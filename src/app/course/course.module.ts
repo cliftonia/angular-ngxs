064e740419d4d3ca4a1cee56873dcd5cd23d9a15
Clifton <clifton.baggerman@me.com>
2020-10-02 13:25:26 +1000

    added courses-list component

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseService } from './services/course.service';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CoursesListComponent],
   imports: [CommonModule],
   providers: [CourseService],
 })
